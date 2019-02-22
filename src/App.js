02ab82e209871cad17216d70ce963a7cdad78828
pranav1234 <pranav0492@gmail.com>
2019-02-22 19:28:14 +0530

    add card view

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import { Provider } from 'react-redux';
+import { Route} from 'react-router';
+import { BrowserRouter  } from "react-router-dom";
+import  store from "./configureStore";  
+import rootReducer from './reducer';
+import {createLogger} from 'redux-logger';
+import thunk from 'redux-thunk';
+import axios from 'axios';
+import {fetchInitialData} from './actions';
 
+import { createStore, applyMiddleware } from 'redux';
+const middlewares = [thunk];
+middlewares.push(createLogger());
+
+
+
+applyMiddleware(...middlewares)
+
+
+
+const finalStore  = createStore(rootReducer,applyMiddleware(...middlewares));
+finalStore.dispatch(fetchInitialData());
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Provider store={finalStore}>
+          <BrowserRouter  >
+            <Route path="/" component={Home} />
+          </BrowserRouter>  
+        </Provider>
     );
   }
 }
